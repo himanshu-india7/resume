541b199a0d250c7f026c0c9fefe93b9282109930
himanshu <himanshu.singh@enercross.com>
2026-08-18 21:02:42 +0530

    Add resume website

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth navigation is handled by CSS.
+// This file is kept ready for future interactions.
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Himanshu Singh Resume Loaded");
+
+});
